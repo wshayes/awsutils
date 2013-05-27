2ee671e014f4d665537766fc3eb4ee7ecca6d413
William Hayes <william.s.hayes@gmail.com>
2013-05-27 19:24:52 -0400

    added print messages

--- backupInstances.py
+++ backupInstances.py
@@ -11,15 +11,16 @@ imag -- the imaginary part (default 0.0)
 
 import sys
 import boto
-# from boto.exception import EC2ResponseError
 import traceback
 
 ec2 = boto.connect_ec2()
 
+# uses ~/.boto file for AWS keys
 
 def backup():
     vols = {}
 
+    print "Collecting running Instances\n"
     reservations = ec2.get_all_instances(filters={'instance-state-name': 'running'})
     instances = [i for r in reservations for i in r.instances]
 
@@ -29,9 +30,10 @@ def backup():
             vols[volume] = (instance, volume.attach_data.device)
 
     for v in vols:
-        print "Volume: %s  Device: %s  Instance: %s" % (v, vols[v][1], vols[v][0])
+        print "Creating Snapshot of Volume: %s  Device: %s  Instance: %s" % (v, vols[v][1], vols[v][0])
         v.create_snapshot()
 
+    print "Trimming snapshots"
     ec2.trim_snapshots(hourly_backups=8, daily_backups=7, weekly_backups=4)
 
 
